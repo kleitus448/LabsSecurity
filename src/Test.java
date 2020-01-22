29cdd93a3269b73c2e8f54931cfa88ac6fdf8c62
TonyM <kleitus448@gmail.com>
2020-01-22 20:04:24 +0300

    Delete All
    Delete All

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-import java.math.BigInteger;
-
-public class Test {
-    public static void main(String[] args) {
-        char c = 'e';
-        System.out.println(BigInteger.valueOf(c));
-        System.out.println(Integer.valueOf(c));
-    }
-}
